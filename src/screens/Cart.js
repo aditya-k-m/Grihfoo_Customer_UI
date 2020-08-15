fea44bdeb2b1c30f0d8ee37af5a3a5ae8e8e6e9e
Harsh690 <62766144+Harsh690@users.noreply.github.com>
2020-08-15 21:18:41 +0530

    Update Cart and profile content

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,13 +1,146 @@
-import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import React, { Component } from 'react';
+import {StyleSheet,Image,ScrollView} from 'react-native';
+import { Input,Item,Container,Textarea,Form,Header,Content,Left, Body, Right, Title, Text, View} from 'native-base';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Button } from 'react-native-paper';
 
-export default class Cart extends Component{
-    render()
+
+
+export default class HeaderTitleExample extends Component {
+    show1 =()=>
     {
-        return (
-            <View style={{flex:1,justifyContent: 'center',alignItems: 'center'}}>
-                <Text style = {{fontSize:40}}>CartScreen</Text>
-            </View>
-        )
+        alert('Clicked')
     }
+    clicked =()=>
+    {
+        alert('Clicked')
+    }
+  render() {
+    return (
+      <Container >
+        
+        <Header style={styles.container}>
+          <Left/>
+          <Body >
+            <Title style={styles.head}>Your Cart</Title>
+            <Image style={styles.image}source={require('../assets/HARSH.png')}/>
+          </Body>
+          <Right />
+        </Header>
+        <Header style={styles.itemname}>
+            <Text style={styles.item}>Item Name</Text>
+            <Text style={styles.price}>Price</Text>
+        </Header>
+        <Content style={styles.bottom}>
+        <Text >
+                    
+                    Chole Bhature * 4{'\n'}{'\n'}{'\n'}
+                    Dal Makhani * 2{'\n'}{'\n'}{'\n'}
+                    Mango Pickle * 1{'\n'}{'\n'}{'\n'}
+        </Text>
+        <Text style={styles.priced}>
+                    
+                    100{'\n'}{'\n'}{'\n'}
+                    150{'\n'}{'\n'}{'\n'}
+                    200{'\n'}{'\n'}{'\n'}
+        </Text>
+        <Text style={styles.bar}>------------------------------------------------------------------------------------------------</Text>
+       </Content> 
+       
+       <Text style={styles.instructions}>Add Instructions To the Homemaker</Text>
+       
+         <Content style={styles.box}>
+           <Item style={{left:110,marginRight:250}}>
+           <Text style={styles.third}>
+                    1.Food Needs to be spicy{'\n'}
+                    2.Food Needs to be hot and Fresh{'\n'}
+                    3.Less oily and more salty food{'\n'}
+                </Text>
+            {/* <Input style={styles.third}  /> */}
+           </Item> 
+        </Content>
+        
+        <Button onPress={this.show1} style={styles.last}><Text style={{color:'white'}}>Add More Items</Text></Button>
+        
+        <Button onPress={this.show1}style={styles.secondlast}><Text style={{color:'white'}}>Proceed to Request Homemaker</Text></Button>
+      </Container>
+    );
+  }
 }
+
+const styles =StyleSheet.create({
+     container:{
+         backgroundColor:'#006a71', 
+     },
+    head:{
+        right:50,
+        top:25,
+        
+    },
+    image:{
+        height:50,
+        width:50,
+        
+        borderRadius:200,
+        marginLeft:200,
+        marginBottom:5,
+        borderWidth:10,
+        left:65,
+        bottom:12
+        
+    },
+    itemname:{
+        top:30,
+        backgroundColor:'white',
+    },
+    item:{
+        top:10,
+        right:130
+    },
+    price:{
+         top:10,
+         left:120 
+    },
+    bottom:{
+        top:45,
+        left:10,
+    },
+    priced:{
+        left:340,
+        bottom:191
+    },
+    bar:{
+        bottom:240,   
+    },
+    box:{
+      top:20,
+      
+    },
+    instructions:{
+        left:65,
+    },
+    last:{
+        bottom:30,
+        right:10,
+        backgroundColor:'#006a71',
+        marginLeft:110,
+        marginRight:115,
+        color:'white'
+    },
+    secondlast:{
+        backgroundColor:'#006a71',
+        marginLeft:40,
+        marginRight:35,
+        right:10,
+        bottom:15,
+    },
+    third:{
+        borderWidth: 2,
+        marginLeft:-50,
+        marginRight:-70,
+      
+        padding:20,
+        paddingRight:9,
+        paddingLeft:35,
+    }
+})
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
-import {View,Text,Button,StyleSheet} from 'react-native';
-import HyperLink from 'react-native-hyperlink';
+import {View,Text,Button,StyleSheet,TouchableOpacity,FlatList,Image,SafeAreaView,ScrollView} from 'react-native';
+
+// import App from './Hello';
+
 
 
 export default class Profile extends Component{
@@ -15,98 +17,248 @@ export default class Profile extends Component{
     render()
     {
         return (
-        <View>     
-            <View style={styles.pending} >
-                <Text><Button title='Pending' ></Button></Text>
+        
+        <View style={styles.container}>
+            <ScrollView style={styles.scroll}>    
+            <View style={styles.head}>
+                <Text style={styles.hi}>Hi,Harshit!</Text> 
+
+                {/* Picture */}
+                <Image style={styles.image}source={require('../assets/HARSH.png')}/>
             </View>
-            <View style={styles.customerSuport} >     
-                <Text ><Button title='Customer Support' /></Text>
-            </View>
-            <View style={styles.Logout} >
-                <Text><Button title='Logout'></Button></Text>
-            </View>
-            <View style={styles.ongoing}>
-                <Text><Button title='Ongoing'></Button></Text>
-            </View>
-            <View style={styles.past}> 
-                <Text><Button title='Past'></Button></Text>
-            </View>
-            <Text style={styles.faq}>
-                FAQs
-            </Text>
-            <View style={styles.textbox}>
-                <Text style={{left:7}}>
-                    A paragraph of text with an unassigned HyperLink.{'\n'}
-                    A second row with a web link
-                </Text>
-            </View>
-            <Text style = {styles.order}>Your Orders</Text>
-            <View style={styles.upper}>
-                <Text style={{left:7,top:10}}>
+            
+            <View style={styles.box}>
+                {/* Personal Information Heading */}
+                <Text style={styles.top}>Personal Information</Text>
+                {/* Personal Information Tab */}
+            
+                <Text style={styles.bottom}>
+                    
                     Name: Harshit Choudhary{'\n'}
                     Address:204A Nagenahalli Bangalore{'\n'}
                     Email:harshorton@gmail.Com{'\n'}
                     Phone: 9148055977{'\n'}
+                    
+                </Text>
+            
+            </View>
+            
+
+            <View style={styles.box}>
+                <Text style={styles.top}>Order Details</Text>
+                <View style={styles.side}>
+                {/* Pending Orders Tab */}
+
+                <TouchableOpacity onPress={this.clicked} style={styles.order}>
+
+                <Text style={styles.text}>Pending Orders</Text>
+
+                </TouchableOpacity>     
+
+                {/* Ongoing Orders Tab */}
+                <TouchableOpacity onPress={this.clicked} style={styles.order}>
+
+                <Text style={styles.text}>Ongoing Orders</Text>
+
+                </TouchableOpacity>   
+
+                {/* Past Orders Tab */}
+
+                <TouchableOpacity onPress={this.clicked} style={styles.order}>
+
+                <Text style={styles.text}>Past Orders</Text>
+
+                </TouchableOpacity> 
+                </View>
+            
+            </View>
+
+           
+
+           
+            <View style={styles.box}>
+                {/* Faq Text */}
+                <Text style={styles.top}>
+                    FAQs
+                </Text>
+
+
+                {/* Faqs Tab */}
+                <Text style={styles.bottom}>
+                    A paragraph of text with an unassigned HyperLink.{'\n'}
+                    A second row with a web link
                 </Text>
             </View>
-            <Text style={{fontSize:22,bottom:427}}>Personal Information</Text>
-        </View>
+            
+            
+            {/* Customer Support Tab  */}
+
+            <TouchableOpacity onPress={this.clicked} style={styles.order}>
+
+            <Text style={styles.text}>Customer Support</Text>
+
+            </TouchableOpacity>  
+
+             {/* LogOut Tab  */}
+            
+             <TouchableOpacity onPress={this.show1} style={styles.order}>
+
+            <Text style={styles.text}>Logout</Text>    
+
+            </TouchableOpacity> 
+
+
+
+            </ScrollView>
+            </View>
+        
         )
     }
 }
 const styles = StyleSheet.create({
-    customerSuport:{
+    container:{
+        justifyContent:'flex-start',
+        alignItems:'flex-start',
         flex:1,
-        right: 20,
-        top: 345,
-        fontSize: 50,
-        margin: 30,
-    },
-    Logout:{
-        flex:1,
-        right:10,
-        top: 350,
-        fontSize: 50,
-        margin:20,
-       
-    },
-    textbox :{
-        height: 120,
-        borderTopWidth: 2,
-        borderBottomWidth:2,
-        top : 55,
-        backgroundColor:'#cdb30c',
-    },
-    faq:{
-        top:50,
-        left:8,
-    },
-    pending:{
-        top:205,
-        left:10,
-        marginTop:5,
+        width:'100%',
+        // backgroundColor:"rgba(80,250,20,0.1)",
 
     },
-    ongoing:{
-        left: 145,
-        top:70,
+    head:{
+        width:'100%',
+        height:'20%',
+        padding:10,
+        fontSize:200,
+        flexDirection:'row',
+        justifyContent:"space-evenly",
+        alignItems:"center",
+        backgroundColor:"rgba(20,80,90,1)"
     },
-    past:{
-        left:290,
-        top:35
+    hi:{
+        textAlign:"left",
+        margin:10,
+        fontSize:20,
+        color:"white",
+        fontWeight:"bold",
+        letterSpacing:2,
+        textTransform:"capitalize"
     },
-    order:{
-        bottom:180,
-        left: 114,
-        fontSize: 25,
+    image:{
+        height:100,
+        width:100,
+        backgroundColor:'#DDDDDD',
+        borderRadius:200,
+        marginLeft:120,
+        borderWidth:10,
         
     },
-    upper:{
-        height: 90,
-        borderTopWidth: 2,
-        borderBottomWidth:2,
-        bottom: 310,
-        backgroundColor:'#cdb30c',
+    box:{
+        width:'100%',
+    
+    },
+    top:{
+        backgroundColor:"rgba(20,80,90,0.75)",
+        margin:5,
+        padding:10,
+        // width:"100%",
+        fontWeight:"bold"
+    },
+    bottom:{
+        padding:10,
+        height:100
+    },
+    side:{
+        flexDirection:'row',
+        justifyContent:"space-evenly",
+        alignItems:"center",
+        padding:20,
+    },
+    order:{
+        width:"35%",
+        height:35,
+        padding:5,
+        borderWidth:1,
+        marginVertical:5,
+        marginHorizontal:20,
+        backgroundColor:"#006666",
+        elevation:5,
+        // backgroundColor:"rgba(80,250,200,0.5)",
+        borderRadius:5,
+       
+    
+    },
+    scroll:{
+        width:"100%"
+    },
+    text:{
+        color:"white",
+        fontWeight:"bold",
     }
+    // {fontSize:22,bottom:325,left:4}
+    // customerSuport:{
+        
+    //     left:6,
+    //     top: 450,
+    //     fontSize: 50,
+    //     marginRight: 230,
+    //     backgroundColor: "#DDDDDD",
+    // },
+    // hi:{
+    //    bottom:350,
+    //    fontSize:25,
+    //    left:5
+    // },
+    // Logout:{
+    //    left:10,
+    //    top:540,
+    //    backgroundColor: "#DDDDDD",
+    //    marginRight:320,
+       
+    // },
+    // textbox :{
+    //     height: 120,
+    //     borderTopWidth: 2,
+    //     borderBottomWidth:2,
+    //     top: 180,
+    //     backgroundColor:'#00bcd4',
+      
+    // },
+    // faq:{
+    //     top:173,
+    //     left:6,
+    //     fontSize:22,
+    // },
+    // pending:{
+    //     top:181,
+    //     left:6,
+    //     marginRight:285,
+    //     backgroundColor: "#DDDDDD",
+
+    // },
+    // ongoing:{
+    //     top:157,
+    //     left:140,
+    //     marginRight:285,
+    //     backgroundColor: "#DDDDDD",
+    // },
+    // past:{
+    //     top:134,
+    //     left:275,
+    //     marginRight:285,
+    //     backgroundColor: "#DDDDDD",
+    // },
+    // order:{
+    //     top:25,
+    //     left: 114,
+    //     fontSize: 25,
+        
+    // },
+    // upper:{
+    //     height: 90,
+    //     borderTopWidth: 2,
+    //     borderBottomWidth:2,
+    //     bottom: 165,
+    //     backgroundColor:'#00bcd4',
+    // }
     
 })
